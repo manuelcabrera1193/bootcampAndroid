953a1093c2d3688197e25471e419d57dab9436be
apachecv <apachecv@emeal.nttdata.com>
2022-08-26 08:22:21 -0500

    Cambio

---
+++ app/src/main/java/com/bootcamp/nttdata/bootcampandroid/LoginActicity.kt
@@ -0,0 +1,11 @@
+package com.bootcamp.nttdata.bootcampandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActicity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_acticity)
+    }
+}
